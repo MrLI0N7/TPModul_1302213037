94ac5f2f9d8209341df2b9563a4b608b8fb369c4
MrLI0N7 <93116969+MrLI0N7@users.noreply.github.com>
2023-03-12 21:19:14 +0800

    Inisialisasi Project

---
+++ TP_MOD4_1302213037/KodePos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_MOD4_1302213037
+{
+    internal class KodePos
+    {
+        public enum Kelurahan
+        {
+            Batununggal, Kujangsari, Mengger, Wates,
+            Cijaura, Jatisar, Margasari, Sekejati, Kebonwaru, Maleer, Samoja
+        }
+        public int getKodePos(Kelurahan k)
+        {
+            int[] kode = { 40266, 40287, 40267, 40287, 40286, 40286, 40286, 40272, 40274, 40273 };
+
+            return kode[(int)k];
+        }
+    }
+}
---
+++ TP_MOD4_1302213037/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TP_MOD4_1302213037;
+
+internal class Program
+{
+    public static void Main(String[] args)
+    {
+        // Soal 1 Table-Driven
+        KodePos TablePos = new KodePos();
+        KodePos.Kelurahan kelurahan = KodePos.Kelurahan.Maleer;
+        int kodePos = TablePos.getKodePos(kelurahan);
+        Console.WriteLine("KodePos dari kelurahan " + kelurahan + " adalah" + kodePos);
+
+    }
+}
